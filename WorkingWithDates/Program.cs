eb58ec2674e55e8bd7657e730a37a4d8a6090215
Gjurgica Anastasovska <gurgica78@gmail.com>
2019-03-07 19:49:38 +0100

    third commit

--- WorkingWithDates/Program.cs
+++ WorkingWithDates/Program.cs
@@ -12,8 +12,8 @@ namespace WorkingWithDates
         {
             Console.WriteLine(DateTime.Now.AddDays(3));
             Console.WriteLine(DateTime.Now.AddMonths(1));
-            Console.WriteLine(new DateTime(2019, 04, 10));
-            Console.WriteLine(new DateTime(2018, 01, 07));
+            Console.WriteLine(DateTime.Now.AddDays(3).AddMonths(1));
+            Console.WriteLine(DateTime.Now.AddYears(-1).AddMonths(-2));
             Console.WriteLine(DateTime.Now.ToString("MMMM"));
             Console.WriteLine(DateTime.Now.Year);
             Console.ReadLine();
