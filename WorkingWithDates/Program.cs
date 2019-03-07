20169204f1501cd9f80cd1a91fdb6b270ddde748
Gjurgica Anastasovska <gurgica78@gmail.com>
2019-03-07 17:22:00 +0100

    first commit

---
+++ WorkingWithDates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.AddDays(3));
+            Console.WriteLine(DateTime.Now.AddMonths(1));
+            Console.WriteLine(new DateTime(2019, 04, 10));
+            Console.WriteLine(new DateTime(2018, 01, 07));
+            Console.WriteLine(DateTime.Now.ToString("MMMM"));
+            Console.WriteLine(DateTime.Now.Year);
+            Console.ReadLine();
+
+        }
+       
+    }
+}
---
+++ ageCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ageCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your birthday in this Format(YYYY-MM-DD):");
+            string inputDate = Console.ReadLine();
+            DateTime birthday = Convert.ToDateTime(inputDate);
+            if (birthday.Day == DateTime.Now.Day)
+            {
+                Console.WriteLine("Happy Birthday");
+            }
+            Console.WriteLine(ageCalculator(birthday));
+            Console.ReadLine();
+        }
+        public static int ageCalculator(DateTime date)
+        {
+             DateTime today = DateTime.Now;
+             int year = today.Year;
+             return year - date.Year;
+        }
+    }
+}
