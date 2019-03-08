fe4152068ae39c82cf88ffafc0db6a3e6dac1619
Gjurgica Anastasovska <gurgica78@gmail.com>
2019-03-08 11:11:30 +0100

    fourth commit

--- ageCalculator/Program.cs
+++ ageCalculator/Program.cs
@@ -13,25 +13,51 @@ namespace ageCalculator
             Console.WriteLine("Enter your birthday in this Format(YYYY-MM-DD):");
             string inputDate = Console.ReadLine();
             DateTime birthday = Convert.ToDateTime(inputDate);
-            if (birthday.Day == DateTime.Now.Day && birthday.Month == DateTime.Now.Month)
-            {
-                Console.WriteLine("Happy Birthday");
-            }
-            else if(birthday.Day - DateTime.Now.Day == 1 && birthday.Month == DateTime.Now.Month){
-                Console.WriteLine("Happy birthday, little late");
-                                        }
-            else if(birthday.Day - DateTime.Now.Day == -1 && birthday.Month == DateTime.Now.Month)
-            {
-                Console.WriteLine("Just one day left to your birthday");
-            }
             Console.WriteLine(ageCalculator(birthday));
             Console.ReadLine();
         }
-        public static int ageCalculator(DateTime date)
+        public static string ageCalculator(DateTime date)
         {
+            //get today date
              DateTime today = DateTime.Now;
+            //get today year
              int year = today.Year;
-             return year - date.Year;
+            //substracting today year from birth year
+             int calculateYear = year - date.Year;
+            //get today mounth
+            int mounth = today.Month;
+            //calculate how much mounth 
+            int calculateMounth;
+            if (mounth > date.Month)
+            {
+                calculateMounth = mounth - date.Month;
+            }else if (mounth < date.Month)
+            {
+                calculateYear--;
+                calculateMounth = Math.Abs(mounth - date.Month);
+
+            }
+            else
+            {
+                calculateMounth = 0;
+            }
+            //get today day
+            int day = today.Day;
+            //calculate hou much days
+            int calculateDays;
+            if(day > date.Day)
+            {
+                calculateDays = day - date.Day;
+            }else if(day < date.Day)
+            {
+                calculateMounth--;
+                calculateDays = Math.Abs(day - date.Day);
+            }
+            else
+            {
+                calculateDays = 0;
+            }
+            return "You are " + calculateYear + " year " + calculateMounth + " mounth " + calculateDays + " days";
         }
     }
 }
